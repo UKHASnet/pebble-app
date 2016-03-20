3042a6e2aeaaebf4504bcf1b8ebdc4c8ba0cb08e
Ben Oxley <ben.oxley@gmail.com>
2016-03-20 18:43:42 +0000

    Sorted interval messaging

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,3 @@
-// Function to send a message to the Pebble using AppMessage API
-// We are currently only sending a message using the "status" appKey defined in appinfo.json/Settings
-function sendMessage() {
-	Pebble.sendAppMessage({"status": 1}, messageSuccessHandler, messageFailureHandler);
-}
-
 // Called when the message send attempt succeeds
 function messageSuccessHandler() {
   console.log("Message send succeeded.");  
@@ -18,7 +12,8 @@ function messageFailureHandler() {
 // Called when JS is ready
 Pebble.addEventListener("ready", function(e) {
   console.log("JS is ready!");
-  sendMessage();
+  getData();
+  setInterval(getData, 60000);
 });
 												
 // Called when incoming message from the Pebble is received
