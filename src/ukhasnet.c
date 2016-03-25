418ae094b535c0909cc5f849c280c414ba6f690e
Ben Oxley <ben.oxley@gmail.com>
2016-03-25 13:31:39 +0000

    Trying something else

--- src/ukhasnet.c
+++ src/ukhasnet.c
@@ -38,7 +38,7 @@ static void window_init(Window *window) {
 
 static void inbox_recieved_callback(DictionaryIterator *iter, void *context) {
 	static char str[60];
-	snprintf(str, sizeof(str), "Receiving Message from dictionary of size %zu\n", dict_size(iter));
+	snprintf(str, sizeof(str), "Receiving Message from dictionary of size %lu\n", (unsigned long)dict_size(iter));
 	printf(str);
 		// Does this message contain a temperature value?
 	Tuple *message_tuple = dict_find(iter, message);
